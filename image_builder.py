b6637201acb67d9bda4c1ec4fd4559c3d842e896
rtohid <tohid.rod@gmail.com>
2020-05-17 09:50:13 +0000

    Formatting: flake8.

--- image_builder.py
+++ image_builder.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 __license__ = """Copyright (c) 2020 Karame
 
 Distributed under the Boost Software License, Version 1.0. (See accompanying
@@ -13,54 +12,62 @@ import os
 parser = argparse.ArgumentParser(description='build an environment.')
 # set the linux dirtributions
 distro = parser.add_mutually_exclusive_group()
-distro.add_argument('-os', "--os",
-                    choices=['ubuntu', 'fedora'],
-                    default='fedora',
-                    help='builds the image based on selected OS (default: frdora)'
-                    )
+distro.add_argument(
+    '-os',
+    "--os",
+    choices=['ubuntu', 'fedora'],
+    default='fedora',
+    help='builds the image based on selected OS (default: frdora)')
 
 # set the build type
-parser.add_argument('-b', "--build",
-                    choices=['debug', 'release'],
-                    default='debug',
-                    help='builds in "debug" or "release" mode (default: debug)'
-                    )
-parser.add_argument('-i', "--install",
-                    choices=['debug', 'release'],
-                    default='debug',
-                    help="builds in 'debug' or 'release' mode (default: debug)"
-
-                    )
+parser.add_argument(
+    '-b',
+    "--build",
+    choices=['debug', 'release'],
+    default='debug',
+    help='builds in "debug" or "release" mode (default: debug)')
+parser.add_argument(
+    '-i',
+    "--install",
+    choices=['debug', 'release'],
+    default='debug',
+    help="builds in 'debug' or 'release' mode (default: debug)")
 
 # add user
-parser.add_argument('-u', "--user",
+parser.add_argument('-u',
+                    "--user",
                     choices=['stellar', 'karame'],
                     default='stellar',
-                    help='adding user to the image build (default: stellar)'
-                    )
+                    help='adding user to the image build (default: stellar)')
 
 # set the env
 parser.add_argument("--env",
                     choices=['python', 'c++'],
-                    help=' building the image based on python or c++ '
-                    )
+                    help=' building the image based on python or c++ ')
 args = parser.parse_args()
 package_manager = "dnf" if args.os == "fedora" else "apt"
 
-fedora_dep = ["python", "sudo", "git",
-              "boost-devel", "wget curl", "environment-modules", "findutils", "cmake", "gdb", "gcc-c++", "openmpi",
-              "numpy", "bzip2-devel", "fontconfig-devel",
-              "freetype-devel", "fribidi-devel", "harfbuzz-devel", "jansson-devel", "lame-devel", "lbzip2", "libass-devel",
-              "libogg-devel", "libsamplerate-devel", "libtheora-devel", "libtool", "libvorbis-devel", "libxml2-devel",
-              "libvpx-devel", "m4", "make", "meson", "nasm", "ninja-build", "numactl-devel", "opus-devel", "patch",
-              "speex-devel", "tar", "xz-devel", "zlib-devel"]
+fedora_dep = [
+    "python", "sudo", "git", "boost-devel", "wget curl", "environment-modules",
+    "findutils", "cmake", "gdb", "gcc-c++", "openmpi", "numpy", "bzip2-devel",
+    "fontconfig-devel", "freetype-devel", "fribidi-devel", "harfbuzz-devel",
+    "jansson-devel", "lame-devel", "lbzip2", "libass-devel", "libogg-devel",
+    "libsamplerate-devel", "libtheora-devel", "libtool", "libvorbis-devel",
+    "libxml2-devel", "libvpx-devel", "m4", "make", "meson", "nasm",
+    "ninja-build", "numactl-devel", "opus-devel", "patch", "speex-devel",
+    "tar", "xz-devel", "zlib-devel"
+]
 
-ubuntu_dep = ["autoconf", "automake", "sudo"
-              "build-essential", "autopoint", "cmake", "git", "libass-dev", "libbz2-dev", "libfontconfig1-dev",
-              "libfreetype6-dev", "libfribidi", "libharfbuzz-dev", "libjansson-dev", "liblzma-dev", "libmp3lame-dev",
-              "libnuma-dev", "libogg-dev", "libopus-dev", "libsamplerate-dev", "libspeex-dev", "libtheora-dev",
-              "libtool", "libtool-bin", "libvorbis-dev", "libx264-dev", "libxml2-dev", "libvpx-dev", "m4", "make",
-              "nasm", "ninja-build", "patch", "pkg-config", "python", "tar", "zlib1g-dev", "meson", "python3-pip"]
+ubuntu_dep = [
+    "autoconf", "automake", "sudo"
+    "build-essential", "autopoint", "cmake", "git", "libass-dev", "libbz2-dev",
+    "libfontconfig1-dev", "libfreetype6-dev", "libfribidi", "libharfbuzz-dev",
+    "libjansson-dev", "liblzma-dev", "libmp3lame-dev", "libnuma-dev",
+    "libogg-dev", "libopus-dev", "libsamplerate-dev", "libspeex-dev",
+    "libtheora-dev", "libtool", "libtool-bin", "libvorbis-dev", "libx264-dev",
+    "libxml2-dev", "libvpx-dev", "m4", "make", "nasm", "ninja-build", "patch",
+    "pkg-config", "python", "tar", "zlib1g-dev", "meson", "python3-pip"
+]
 message = f"""
 From {args.os} 
 RUN {package_manager} -y update
@@ -87,11 +94,10 @@ with open("Dockerfile", 'w') as file:
 docker_client = docker.from_env()
 
 image_tag = "build"
-image_name = "{image_name}:{tag}".format(
-    image_name='dockerfile.python', tag=image_tag)
+image_name = "{image_name}:{tag}".format(image_name='dockerfile.python',
+                                         tag=image_tag)
 # #image_name = "dockerfile.python:build"
-a = docker_client.images.build(
-    path='/home/karame/repos/python-repo/', tag=image_name)
-
+a = docker_client.images.build(path='/home/karame/repos/python-repo/',
+                               tag=image_name)
 
 print(a)
